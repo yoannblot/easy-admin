e6a1a3ce2dd9d6aa9d01d3c78dd2e993d2ddb3a1
yoann <yoann.blot@gmail.com>
2021-04-02 11:25:43 +0200

    tests: form tag factory

---
+++ tests/Unit/Application/FormTagFactoryTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Application;
+
+use EasyAdmin\Application\FormTagFactory;
+use PHPUnit\Framework\TestCase;
+use Tests\Builder\Form\Item\ItemStructureBuilder;
+
+final class FormTagFactoryTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_creates_a_form(): void
+    {
+        $structure = (new ItemStructureBuilder())->withName('name')->build();
+
+        $formTag = (new FormTagFactory())->getCreateFormTag($structure);
+        self::assertSame('POST', $formTag->getMethod());
+        self::assertSame('create-form-name', $formTag->getName());
+        self::assertSame('create-form-name', $formTag->getId());
+        self::assertSame('create/name', $formTag->getAction());
+    }
+}
