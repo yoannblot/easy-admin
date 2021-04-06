7a11cbb818d7d4b0553fcd4c57d882e546adbdb2
yoann <yoann.blot@gmail.com>
2021-04-06 18:48:57 +0200

    refactor: remove formType from form factory in order to simplify it

--- src/Application/Controller/CreateController.php
+++ src/Application/Controller/CreateController.php
@@ -6,6 +6,7 @@ namespace EasyAdmin\Application\Controller;
 
 use EasyAdmin\Application\Loader\ConfigurationLoader;
 use EasyAdmin\Domain\Form\FormType\FormFactory;
+use EasyAdmin\Domain\Form\Item\ItemStructure;
 use EasyAdmin\Domain\Parser\Parser;
 use EasyAdmin\Infrastructure\Viewer\Html\FormType\FormViewer;
 use Symfony\Component\HttpFoundation\Request;
@@ -40,20 +41,28 @@ final class CreateController implements Controller
 
     public function __invoke(Request $request): Response
     {
-        if ($request->getMethod() === Request::METHOD_POST) {
-            // TODO retrieve values
-            $values = $request->request->all();
-        }
-
         $itemName = $request->query->get('type');
+        $itemStructure = $this->parser->parse($this->loader->getFilePath($itemName));
+
+        if ($request->getMethod() === Request::METHOD_POST) {
+            return $this->createItem($itemStructure, $request->request->all());
+        }
 
         return new Response(
             $this->formViewer->toHtml(
                 $this->formFactory->createForm(
-                    $this->parser->parse($this->loader->getFilePath($itemName)),
-                    'create'
+                    $itemStructure
                 )
             ), Response::HTTP_OK
+        );
+    }
+
+    private function createItem(ItemStructure $itemStructure, array $values): Response
+    {
+        // TODO create item from values
+
+        return new Response(
+            'creation ok', Response::HTTP_OK
         );
     }
 }
--- src/Domain/Form/FormType/FormFactory.php
+++ src/Domain/Form/FormType/FormFactory.php
@@ -8,7 +8,6 @@ use EasyAdmin\Application\FormTagFactory;
 use EasyAdmin\Domain\Form\Button\CreateButton;
 use EasyAdmin\Domain\Form\Item\ItemStructure;
 use EasyAdmin\Domain\I18N\Translator;
-use InvalidArgumentException;
 
 final class FormFactory
 {
@@ -26,25 +25,18 @@ final class FormFactory
 
     /**
      * @param ItemStructure $itemStructure
-     * @param string        $formType
      *
      * @return Form
-     *
-     * @throws InvalidArgumentException
      */
-    public function createForm(ItemStructure $itemStructure, string $formType): Form
+    public function createForm(ItemStructure $itemStructure): Form
     {
-        if ($formType === 'create') {
-            return new CreateForm(
-                $this->formTagFactory->getCreateFormTag($itemStructure),
-                $itemStructure,
-                new CreateButton(
-                    'create-' . $itemStructure->getName(),
-                    $this->translator->translate('submit')
-                )
-            );
-        }
-
-        throw new InvalidArgumentException(sprintf('Invalid form type "%s"', $formType));
+        return new CreateForm(
+            $this->formTagFactory->getCreateFormTag($itemStructure),
+            $itemStructure,
+            new CreateButton(
+                'create-' . $itemStructure->getName(),
+                $this->translator->translate('submit')
+            )
+        );
     }
 }
