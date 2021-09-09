b176bcf0e80fb8d7c03d2e9892e98a0719e26960
yoann <yoann.blot@gmail.com>
2021-09-09 11:49:23 +0200

    doc: add a contributing section in README.md

--- README.md
+++ README.md
@@ -2,6 +2,28 @@
 
 Easy tools for building an administration linked to a relational database
 
+## Contributing
+
+### Installation
+
+```shell
+composer install
+```
+
+### Testing
+
+```shell
+php vendor/bin/phpunit --configuration phpunit.xml.dist
+```
+
+### Run server
+
+```shell
+php -S "localhost:8080" 
+```
+
+You can now access the back office using http://localhost:8080/?lang=fr&page=list&type=civility
+
 ## Components
 
 A [list of all available components is available here](./doc/components/index.md).
---
+++ bin/output.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+use EasyAdmin\Application\Loader\ConfigurationLoader;
+use EasyAdmin\Application\Router;
+use EasyAdmin\Domain\Database\Connector;
+use EasyAdmin\Domain\I18N\LanguageDetector;
+use EasyAdmin\Domain\I18N\Loader;
+use EasyAdmin\Infrastructure\Database\MySql\MysqlConnexion;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+require __DIR__ . '/../config/autoload.php';
+
+/** @var ContainerBuilder $containerBuilder */
+/** @var Router $configFileToHtml */
+$configFileToHtml = $containerBuilder->get(Router::class);
+
+/** @var ConfigurationLoader $loader */
+$loader = $containerBuilder->get(ConfigurationLoader::class);
+$loader->load(__DIR__ . '/../var/conf');
+
+/** @var Loader $i18nLoader */
+$i18nLoader = $containerBuilder->get(Loader::class);
+$i18nLoader->load(__DIR__ . '/../var/lang');
+
+/** @var Connector $connector */
+$connector = $containerBuilder->get(Connector::class);
+// TODO move to configuration
+$connector->load(new MysqlConnexion('database', 'localhost', 'root', 'password'));
+
+$response = $configFileToHtml->execute();
+
+if ($response instanceof JsonResponse) {
+    echo $response->getContent();
+    exit;
+}
+
+/** @var LanguageDetector $languageDetectory */
+$languageDetectory = $containerBuilder->get(LanguageDetector::class);
+
+$rootUrl = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+$rootUrl .= '?lang=' . $languageDetectory->detect()->getShortName();
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TODO</title>
+</head>
+<body data-root-url="<?= $rootUrl; ?>">
+<?= $response->getContent(); ?>
+<script type="application/javascript"><?= file_get_contents(__DIR__ . '/../js/init.js'); ?></script>
+</body>
+</html>
